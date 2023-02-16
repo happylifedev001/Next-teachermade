198029c7518543c5a69d69f5d5c4ff24aa0cf5f9
happylifedev001 <happylifedev001@gmail.com>
2023-02-17 03:08:24 +0900

    add bootstrap, popper.js, jquery, fontawesome-free

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mb-1" id="app-menu">
+      <nav className="mb-4 navbar navbar-expand-md navbar-dark bg-dark">
+        <div className="container">
+          <a href="https://www.teachermade.com" className="navbar-brand">
+            <img src={logo} alt="Teacher Made" height="45"/>
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="navbar-collapse collapse" id="collapsibleNavbar">
+            <nav className="mr-auto navbar-nav">
+              <a href="/app" className="nav-link">My Activities</a>
+              <button type="button" className="nav-link btn btn-link">
+                <span>New Activity</span>
+              </button>
+            </nav>
+            <nav className="align-items-stretch navbar-nav">
+              <div className="dropdown">
+                <a href="/current" className="mr-3 nav-link dropdown-toggle" data-toggle="dropdown">
+                  <i className="fas fa-user mr-2"></i>
+                  User
+                </a>
+                <div className="dropdown-menu">
+                  <a className="dropdown-item" href="/logout">
+                    <i className="fas fa-sign-out-alt mr-2"></i>
+                    Logout
+                  </a>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
