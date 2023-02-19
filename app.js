80e94257cc9f9e45ba9c2400c07dbac46cd3b728
zenith1995 <versatile.skydev@gmail.com>
2023-02-18 18:13:40 -0800

    bachend start

--- app.js
+++ app.js
@@ -17,4 +17,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const PORT = process.env.PORT || 5000;
+  
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
